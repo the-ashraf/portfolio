9023a896b63c6bc19a4b7498f2f7f9ccb97931c3
Ashraf Ezzat <hot4mix@gmail.com>
2023-06-07 11:23:21 +0300

    change the slider card animations

--- src/components/ProjectsSlider.vue
+++ src/components/ProjectsSlider.vue
@@ -7,27 +7,19 @@ const options = {
     gap: '1rem',
     perPage: 4,
     breakpoints: {
-        320: {
+        640: {
             perPage: 1,
         },
-        640: {
+        1024: {
             perPage: 2,
         },
-        1024: {
-            perPage: 3,
-        },
         1280: {
-            perPage: 4,
+            perPage: 3,
         }
     },
     classes: {
-        // Add classes for arrows.
         arrows: 'splide__arrows absolute -top-12 right-2 inline-block flex gap-4',
         arrow: 'inline-flex z-10 justify-center items-center p-2 w-8 h-8 rounded-full border-2 shadow-lg bg-white disabled:bg-gray-100 disabled:fill-gray-300 fill-gray-500',
-        prev: 'splide__arrow--prev',
-        next: 'splide__arrow--next',
-
-        // Add classes for pagination.
         pagination: 'inline-flex absolute -bottom-8 right-1/2 z-10 flex-wrap justify-center items-center p-0 m-0 translate-x-1/2', // container
         page: 'inline-block bottom-0 p-0 m-1 w-2 h-2 lg:w-4 lg:h-4 rounded-full transition-all duration-200 bg-gray-200 custom-is-active', // each button
     }
@@ -40,14 +32,15 @@ const options = {
         <SplideSlide v-for="client in clients" :key="client.name" class="group">
             <div class="w-full h-full flex flex-col flex-1 bg-white overflow-hidden">
                 <div class="flex flex-col flex-1 border overflow-hidden rounded-md">
-                    <div class="w-full h-48">
+                    <div class="w-full h-48 border-b overflow-hidden">
                         <a :href="'/projects/' + client.slug">
                             <img :alt="client.name" :src="client.featured_image"
-                                 class="block w-full h-full object-top object-cover">
+                                 class="block w-full h-full object-top object-cover transition-transform duration-300 group-hover:scale-105">
                         </a>
                     </div>
-                    <div class="flex-1 p-4 space-y-4 transition-transform duration-300 group-hover:scale-105">
-                        <h5 class="font-bold text-lg font-cursive text-[#485b73] pl-2 border-l-4 border-[#485b73]">
+                    <div class="flex-1 p-4 space-y-4">
+                        <h5 class="relative inline-block pr-4 isolate font-bold text-lg font-cursive text-[#485b73] pl-2 border-l-4 border-[#485b73] group-hover:text-white transition-colors duration-300">
+                            <span class="absolute block inset-0 right-1 group-hover:bg-[#485b73] w-0 group-hover:w-full transition-all duration-300 -z-10"></span>
                             <a :href="'/projects/' + client.slug">
                                 {{ client.name }}
                             </a>
