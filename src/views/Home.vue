0fdbb20f796997625b48468e146aa2b170348ea5
Ashraf Ezzat <hot4mix@gmail.com>
2023-09-24 22:25:51 +0300

    automatically focus input when user clicks the contact button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <script setup>
+import {ref} from "vue";
 import SiteHeader from "../components/SiteHeader.vue"
 import SiteFooter from "../components/SiteFooter.vue"
 import ProjectsSlider from '../components/ProjectsSlider.vue';
 
-function scrollToElement(elementId) {
+const input = ref(null)
+
+const scrollToElement = (elementId, focus = false) => {
 	const element = document.getElementById(elementId);
 	element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  if (focus) {
+    setTimeout(() => element.querySelector('#name').focus(), 400)
+  }
 }
 
 </script>
@@ -34,7 +40,7 @@ function scrollToElement(elementId) {
 				<a class="btn btn-secondary"
 				   href="#projects" @click.prevent="scrollToElement('projects')">My Work</a>
 				<a class="btn btn-primary"
-				   href="#contact" @click.prevent="scrollToElement('contact')">Contact Me</a>
+				   href="#contact" @click.prevent="scrollToElement('contact', true)">Contact Me</a>
 			</div>
 		</div>
 
@@ -96,6 +102,12 @@ function scrollToElement(elementId) {
 									d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23Z"
 									fill="currentColor"></path>
 							</svg>
+						</a>
+
+						<a class="text-[#0077b5] hover:text-[#00a0dc] hover:scale-125 transition-all duration-300"
+						   title="LinkedIn Profile"
+						   href="https://www.linkedin.com/in/aezzat88/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6.94 5a2 2 0 1 1-4-.002a2 2 0 0 1 4 .002ZM7 8.48H3V21h4V8.48Zm6.32 0H9.34V21h3.94v-6.57c0-3.66 4.77-4 4.77 0V21H22v-7.93c0-6.17-7.06-5.94-8.72-2.91l.04-1.68Z"/></svg>
 						</a>
 					</div>
 					<p class="font-light leading-[2] max-w-sm">
