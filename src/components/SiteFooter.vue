7f16b354d9d55161795f6a607b94704d7d4de13f
Ashraf Ezzat <hot4mix@gmail.com>
2023-09-24 22:53:51 +0300

    add current year to the footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,5 +1,5 @@
 <template>
 	<div class="mt-8 text-sm text-gray-500 font-light">
-		&copy; A copyright message for the sake of it, Ashraf Ezzat 2022.
+		&copy; A copyright message for the sake of it, Ashraf Ezzat {{ new Date().getFullYear() }}.
 	</div>
 </template>
